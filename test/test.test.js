dc251b6c35cc65309872771c220f6087117e83a3
Kiti Suupohja <kiti.suupohja@futurice.com>
2024-11-11 13:40:26 +0200

    Add temporary test file for setup

---
+++ test/test.test.js
@@ -0,0 +1,10 @@
+import add from '../src/add.js';
+import assert from 'assert';
+import * as chai from 'chai';
+
+
+describe('test', function () {
+  it('should pass', function () {
+    return true;
+  });
+});
